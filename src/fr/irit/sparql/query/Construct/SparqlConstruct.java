0a598132428b04e6a280ce2f0446330e53f2f7e1
Nicolas Seydoux <nseydoux@laas.fr>
2016-11-11 15:39:28 +0100

    Addition of a construct query

---
+++ src/fr/irit/sparql/query/Construct/SparqlConstruct.java
@@ -0,0 +1,35 @@
+package fr.irit.sparql.query.Construct;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import fr.irit.sparql.query.SparqlQuery;
+
+public class SparqlConstruct extends SparqlQuery{
+	private String construct;
+	
+	public SparqlConstruct(Set<Map.Entry<String, String>> prefix, String from, String construct, String where) {
+		super(prefix, from, where);
+		this.construct = construct;
+	}
+	
+	public SparqlConstruct(Set<Map.Entry<String, String>> prefix, String construct, String where) {
+		super(prefix, "", where);
+		this.construct = construct;
+	}
+	
+	public SparqlConstruct(String construct, String where) {
+		super(new HashSet<Map.Entry<String, String>>(), "", where);
+		this.construct = construct;
+	}
+	
+	public String toString()
+	{
+		
+		return    this.formatPrefixes()
+				+ "CONSTRUCT "+this.construct+"\n"
+				+ (this.getFrom().equals("")?"":" FROM "+this.getFrom()+"\n")
+				+ " WHERE {\n"+this.getWhere()+"}\n";
+	}
+}
